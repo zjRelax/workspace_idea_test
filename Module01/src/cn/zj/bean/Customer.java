09dfbb52b623cb05cce798d0637628b98e4cb257
zjRelax <645455635@qq.com>
2020-05-04 14:00:53 +0800

    Initial commit

---
+++ Module01/src/cn/zj/bean/Customer.java
@@ -0,0 +1,9 @@
+package cn.zj.bean;
+
+public class Customer {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/cn/zj/cn/zj/test/TemplateTest.java
@@ -0,0 +1,22 @@
+package cn.zj.cn.zj.test;
+
+/**
+ * @author zhangjian
+ * @create 2020-05-03-19:21
+ */
+public class TemplateTest {
+   public static final int num = 10;
+   
+           
+   
+           
+    
+            
+
+
+    public static void main(String[] args) {
+        System.out.println("hello Template");
+        System.out.println();
+    }
+
+}
